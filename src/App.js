8dc8e34a0692e3f69ca52ba3e9fb71191491bcef
spr021 <saber.pourrahimi.1999@gmail.com>
2021-11-23 17:52:49 +0330

    get 10 random contries

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import * as React from "react"
+import { useEffect, useState } from "react"
 import Box from "@mui/material/Box"
 import Container from "@mui/material/Container"
+import axios from "axios"
+import getNRandomItem from "./utils/GetNRandomItem"
 
 export default function App() {
+  const [contries, setContries] = useState([])  
+
+  useEffect(() => {
+    axios.get("https://travelbriefing.org/countries.json").then((resp) => {
+      setContries(getNRandomItem(resp.data, 10))
+    })
+  }, [])
+
   return (
-      <main>
-        <Box sx={{ bgcolor: "background.paper", pt: 8, pb: 6 }}>
-          <Container>
-            
-          </Container>
-        </Box>
-      </main>
+    <main>
+      <Box sx={{ bgcolor: "background.paper", pt: 8, pb: 6 }}>
+        <Container>
+          {contries.map((e) => (
+            <p key={e.name}>{e.name}</p>
+          ))}
+        </Container>
+      </Box>
+    </main>
   )
 }
