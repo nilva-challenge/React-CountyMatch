fbe88ff1c75af910cca75ef59a1f2a2d4b77b9be
spr021 <saber.pourrahimi.1999@gmail.com>
2021-11-23 16:35:58 +0330

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div></div>
-}
+import * as React from "react"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
 
-export default App
+export default function App() {
+  return (
+      <main>
+        <Box sx={{ bgcolor: "background.paper", pt: 8, pb: 6 }}>
+          <Container>
+            
+          </Container>
+        </Box>
+      </main>
+  )
+}
